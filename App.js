5d4bbbb44ccdf92698328a3485aa51385df30792
vahehakobyanv <vahehakobyanv@gmail.com>
2017-10-15 01:00:54 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+app.post('/post/plan/shopp/items', function (req,res){
+	console.log('Post method');
+	res.send('some items posted');
+})
+app.get('/get/plan/shopp/items', function (req, res) {
+    res.send('Some Items');
+})
+
+app.get('/user/last', function (req, res) {
+    res.send('Some Items');
+})
+app.listen(3000);
